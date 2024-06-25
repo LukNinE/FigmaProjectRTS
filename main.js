883f2414897b268841e0296b36eb7cb1b3ef0874
LukNinE <darkus303@gmail.com>
2024-06-25 18:01:45 +0200

    all

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 let nav = document.querySelector(".fixed-menu");
-
 nav.addEventListener('click', function(event){
-   let clickTarget = event.target;
-   let activeBtn = document.querySelector('.active');
-   
-   if (clickTarget.classList.contains('nav-link')){
-       clickTarget.classList.add('active');
-       activeBtn.classList.remove('active');
-   }
+  let clickTarget = event.target;
+  let activeBtn = document.querySelector('.active');
+  if (clickTarget.classList.contains('nav-link') ){
+      clickTarget.classList.add('active');
+      activeBtn.classList.remove('active');
+
+      let mobile = document.querySelector('.mobile-menu');
+      if (!mobile.classList.contains('hide')){
+          mobile.classList.add('hide'); 
+      } else {
+        body.classList.remove('hide')
+      }
+      
+  }
+    let body = document.querySelector('body');
+  let mobileMenu = document.querySelector('.mobile-menu');
+  if (!body.classList.contains('odd-scroll') &&
+      !mobileMenu.classList.contains('hide')){
+    body.classList.add('off-scroll');
+  } else {
+    body.classList.remove('off-scroll')
+  }
 });
+
+let mobileMenu = document.querySelector('.mobile-menu');
+ if (!mobileMenu.classList.contains('hide')){
+  console.log('2')
+  let clickTarget1 = document.querySelector('.nav-link')
+  clickTarget1.addEventListener('click', function(){
+    mobileMenu.classList.add('hide')
+  })
+ }
 
 let classLink = '.main-link';
 window.onscroll = function() {
@@ -35,3 +58,11 @@ window.onscroll = function() {
    activeBtn.classList.remove('active')
  }
 };
+
+document.querySelector('.mobile-button') .addEventListener('click', function(event) {
+  document.querySelector('.mobile-menu') .classList.toggle('hide')
+  document.querySelector('.mobile-button').style.display='none'
+});
+
+
+ 
